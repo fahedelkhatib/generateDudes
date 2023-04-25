f61b85a75c6ff7ce216a7f841b02f462baeec4e9
fahedelkhatib <fahedelkhatib12@gmail.com>
2023-04-24 22:36:20 -0700

    made damage and accuracy calculations better

--- battle.cpp
+++ battle.cpp
@@ -8,6 +8,7 @@ public:
 	bool checkIfAlive(RandomDude cont);
 	void proclaimWinner(RandomDude* cont);
 	int calculateDamage(RandomDude* cont1, RandomDude* cont2);
+	int calculateHitChance(RandomDude* cont1, RandomDude* cont2);
 
 private:
 	int result;
@@ -17,7 +18,7 @@ private:
 
 void battleArena::proclaimWinner(RandomDude* cont)
 {
-	std::cout << cont->getFullName() << " wins!!" << std::endl;
+	std::cout << cont->getFullName() << " wins!! They had " << cont->getHealth() << " HP left." << std::endl;
 }
 
 std::string battleArena::oneOnOne(RandomDude* cont1, RandomDude* cont2)
@@ -59,18 +60,8 @@ std::string battleArena::oneOnOne(RandomDude* cont1, RandomDude* cont2)
 bool battleArena::attack(RandomDude* cont1, RandomDude* cont2)
 {
 	// check if attack hits, return false if doesn't hit
-	int agilityDiff = cont1->getAgility() - cont2->getAgility();
-	int avoidChance = 100;
-	if (agilityDiff > 0)
-	{
-		avoidChance = avoidChance - agilityDiff;
-	}
-	else
-	{
-		avoidChance = avoidChance;
-	}
-
-	avoidChance = 50;
+	int avoidChance = this->calculateHitChance(cont1, cont2);
+	//avoidChance = 50;
 
 	if(avoidChance < generateNumber(100))
 	{
@@ -89,17 +80,36 @@ bool battleArena::attack(RandomDude* cont1, RandomDude* cont2)
 	return false;
 }
 
+int battleArena::calculateHitChance(RandomDude* cont1, RandomDude* cont2)
+{
+	int agilityDiff = cont1->getAgility() - cont2->getAgility();
+	int avoidChance = 100;
+	if(agilityDiff > 0)
+	{
+		avoidChance = avoidChance - (agilityDiff / 100);
+	}
+	else
+	{
+		avoidChance = avoidChance - generateNumber(50);
+	}
+	
+	std::cout << "CHANCE TO HIT: " << avoidChance << std::endl;
+	return avoidChance;
+}
+
 int battleArena::calculateDamage(RandomDude* cont1, RandomDude* cont2)
 {
 	int damage = generateNumber(50);
 	int attackMinusDefense = cont1->getAttack() - cont2->getDefense();
 	int ageDifferenceModifier = cont2->getAge() - cont1->getAge();
-	//std::cout << "attack modifier: " << attackMinusDefense << std::endl;
 
 	int finalDamage = damage 
 			+ (ageDifferenceModifier / 3) 
 			- (attackMinusDefense / 100);
-
+	if(finalDamage < 1)
+	{
+		finalDamage = 1;
+	}
 	return finalDamage;
 }
 
--- main.cpp
+++ main.cpp
@@ -9,6 +9,7 @@ int main()
 {
 	srand(time(0));
 
+	/*
 	RandomDude* dudes[100];
 	for (int i = 0; i < 20; ++i)
 	{
@@ -18,9 +19,9 @@ int main()
 	for (int i = 0; i < 20; ++i)
 	{
 		std::cout << dudes[i]->greetingMessage() << std::endl;
-		//std::cout << dudes[i]->getStats() << std::endl;
+		std::cout << dudes[i]->getStats() << std::endl;
 	}
-
+	*/
 	
 	battleArena *arena1 = new battleArena;
 	
